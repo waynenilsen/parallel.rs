21db564bd04830fa07897f819f59128515f13c64
Jorge Aparicio <japaricious@gmail.com>
2015-02-20 20:08:57 +0000

    docs!

---
+++ search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = {};
+searchIndex['parallel'] = {"items":[[0,"","parallel","(Hopefully) Safe fork-join parallelism abstractions"],[5,"divide","","Parallelizes an `operation` over a mutable slice"]],"paths":[]};
+searchIndex['parallel_macros'] = {"items":[[0,"","parallel_macros",""],[14,"execute!","","Executes several closures in parallel"]],"paths":[]};
+
+initSearch(searchIndex);
